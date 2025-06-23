cad8c5abb45a5a43746f4656adf759d8eab26f52
Pranav Patnaik <137661401+iamPranavPatnaik@users.noreply.github.com>
2025-06-22 23:01:13 -0400

    fix: vercel #3

--- pranavres/vite.config.js
+++ pranavres/vite.config.js
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   resolve: {
     alias: {
       '@supabase/supabase-js': '@supabase/supabase-js/dist/module'
@@ -12,10 +20,5 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['@supabase/supabase-js']
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
-    }
   }
 })
